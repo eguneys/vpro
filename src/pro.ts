84d207a90cd0fb6de6d261471a307b38beb88338
Emre Guneyler <eguneys@gmail.com>
2022-05-30 14:06:33 +0300

    set content

--- src/pro.ts
+++ src/pro.ts
@@ -1,6 +1,23 @@
 import { on, createResource, batch, createEffect, createSignal, createMemo, mapArray } from 'solid-js'
 import { read, write, owrite } from './play'
 import tau from './tau'
+import { file_store } from './storage'
+
+const base_source = `
+% V Aid Prolog Language
+
+:- dynamic(white/1).
+:- dynamic(orange/1).
+:- dynamic(black/1).
+
+color(white, Pos) :- white(Pos).
+color(orange, Pos) :- orange(Pos).
+color(black, Pos) :- black(Pos).
+`
+
+function zip<A, B, C>(as: Array<A>, bs: Array<B>, fn: (a: A, b: B) => C) {
+  return as.map((a, i) => fn([a, bs[i]]))
+}
 
 function find_lines(error) {
   if (error?.id === 'line') {
@@ -19,34 +36,30 @@ export class Pro {
 
   constructor() {
 
-    this._source = createSignal(`% V Aid Prolog Language
-:- dynamic(white/1).
-
-  `)
+    this._source = createSignal(file_store.get() || base_source)
 
     this.r_consult = createResource(this._source[0], _pq(_ => tau.consult(_)))
 
-    this.r_whites = createResource("white(X).", _pq(_ => tau.all(_)))
-    let m_whites = createMemo(() => {
+    this.r_whites = createResource("color(Color, X).", _pq(_ => tau.all(_)))
+    let m_colors = createMemo(() => {
       let res = read(this.r_whites)
       if (res) {
-        return res.X
+        return zip(res.Color, res.X.map(_ => _.split('-').join('')), _ => _.join('@'))
       }
       return []
     })
 
     createEffect(() => {
-      let whites = m_whites()
+      let colors = m_colors()
 
 
       if (this.oboard) {
-        this.oboard.squares = whites.map(_ => 'white@' + _.split('-').join(''))
+        this.oboard.squares = colors
       }
 
     })
 
     createEffect(on(this.r_consult[0], () => {
-      console.log('Error', this.r_consult[0].error)
       if (!this.r_consult[0].error) {
         refetch(this.r_whites)
       }
@@ -65,7 +78,20 @@ export class Pro {
   }
 
   on_command(command: string, content: string) {
-    owrite(this._source, content)
+    switch (command) {
+      case ':w':
+        owrite(this._source, content)
+        file_store.set(content)
+        break
+      case ':clear':
+        file_store.remove()
+        owrite(this._source, base_source)
+        this.ovim.content = base_source
+        break
+      default:
+          owrite(this._source, content)
+        break
+    }
   }
 }
 
---
+++ src/storage.ts
@@ -0,0 +1,18 @@
+const make_storage = (storage: Storage) => {
+  let api = {
+
+    get: (k: string) => storage.getItem(k),
+    set: (k: string, v: string) => storage.setItem(k, v),
+    remove: (k: string) => storage.removeItem(k),
+    make: (k: string) => ({
+      get: () => api.get(k),
+      set: (v: any) => api.set(k, v),
+      remove: () => api.remove(k)
+    })
+  }
+  return api
+}
+
+export const storage = make_storage(window.localStorage)
+
+export const file_store = storage.make('vpro-chess.pl')
