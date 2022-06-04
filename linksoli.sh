a8b496bc8588d9e36b28e2f08a60a9f7ba76ff89
Emre Guneyler <eguneys@hotmail.com>
2022-06-04 22:46:10 +0000

    link vchessreplay

--- linksoli.sh
+++ linksoli.sh
@@ -1,6 +1,25 @@
-git clone https://github.com/eguneys/soli2d
-cd soli2d
+git clone https://github.com/eguneys/vchessboard
+cd vchessboard
 yarn install
 yarn build
 yarn link
+
+cd ..
+
+git clone https://github.com/eguneys/vchessreplay
+cd vchessreplay
+yarn install
+yarn build
+yarn link
+
+cd ..
+
+git clone https://github.com/eguneys/ovim
+cd ovim
+yarn install
+yarn build
+yarn link
+
+cd ..
+
 
